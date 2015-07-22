64c5aacb86776a6d6ec0bbbac5436febb2c04913
Clive Chan <doobahead@gmail.com>
2015-07-22 15:05:14 -0400

    Sorta works

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public')); //???
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 app.get('/', function(request, response){
-	res.render('index.html');
+	response.sendFile('index.html');
 });
